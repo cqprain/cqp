e4ff5251f3a72441a15e9ef1f13e8be6d38f84fb
unknown <chenqipeng@ET.com>
2022-10-11 11:48:27 +0800

    init

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require "vendor/autoload.php";
+$obj = new \Cqp\Test();
+$obj->hello();
---
+++ src/cqp/Test.php
@@ -0,0 +1,8 @@
+<?php
+namespace Cqp;
+class Test
+{
+    public function hello(){
+        echo "hello world";
+    }
+}
